8fd83b8684f40b143903dd996aa2e36aa5f6153b
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2025-10-23 21:27:15 -0300

    a

--- cadastro-alunos/src/app/aluno/aluno.spec.ts
+++ cadastro-alunos/src/app/aluno/aluno.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Aluno } from './aluno';
+import { AlunoComponent } from './aluno';
 
 describe('Aluno', () => {
-  let component: Aluno;
-  let fixture: ComponentFixture<Aluno>;
+  let component: AlunoComponent;
+  let fixture: ComponentFixture<AlunoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Aluno]
+      imports: [AlunoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Aluno);
+    fixture = TestBed.createComponent(AlunoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
