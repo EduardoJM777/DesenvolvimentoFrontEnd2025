93eaf4c52f949ec4b02962ab11a0cb339179dc32
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2025-10-28 21:37:38 -0300

    a

--- cadastro-alunos/src/app/app.routes.ts
+++ cadastro-alunos/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AlunoComponent } from './aluno/aluno';
+import { DisciplinaComponent } from './disciplina/disciplina';
+import { ProfessorComponent } from './professor/professor';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'aluno', component: AlunoComponent},
+    {path: 'disciplina', component: DisciplinaComponent},
+    {path: 'professor', component: ProfessorComponent},
+    {path: '', redirectTo: 'aluno', pathMatch: 'full'}
+];
--- cadastro-alunos/src/app/app.ts
+++ cadastro-alunos/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlunoComponent } from './aluno/aluno';
+import { ProfessorComponent } from './professor/professor';
+import { DisciplinaComponent } from './disciplina/disciplina';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AlunoComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
